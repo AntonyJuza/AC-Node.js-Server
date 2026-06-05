e607c324a54c6e314b0dace16fa15dd8b7be63bb
Antony Juza <antonyjuza@gmail.com>
2026-06-06 00:10:33 +0530

    feat: implement mqtt subscriber to process device heartbeat and status updates in postgres

--- src/mqtt/subscriber.js
+++ src/mqtt/subscriber.js
@@ -1,12 +1,66 @@
 const mqttClient = require("./mqttClient");
+const { pool } = require("../../database/postgres");
 
-mqttClient.subscribe("ac/+/status");
-mqttClient.subscribe("ac/+/heartbeat");
+mqttClient.on("connect", () => {
+    console.log("[MQTT] Connected");
 
-mqttClient.on("message", (topic, message) => {
-    console.log(topic);
-    console.log(message.toString());
+    mqttClient.subscribe("ac/+/status");
+    mqttClient.subscribe("ac/+/heartbeat");
 
-    // later:
-    // update MongoDB
+    console.log("[MQTT] Subscribed to topics");
+});
+
+mqttClient.on("message", async (topic, message) => {
+    try {
+        const payload = JSON.parse(message.toString());
+
+        console.log(`[MQTT] ${topic}`);
+        console.log(payload);
+
+        const parts = topic.split("/");
+        const deviceId = parts[1];
+        const eventType = parts[2];
+
+        if (eventType === "heartbeat") {
+
+            await pool.query(`
+                INSERT INTO devices (
+                    device_id,
+                    online,
+                    last_seen
+                )
+                VALUES ($1, TRUE, NOW())
+                ON CONFLICT (device_id)
+                DO UPDATE SET
+                    online = TRUE,
+                    last_seen = NOW()
+            `, [deviceId]);
+
+            console.log(`[DB] Heartbeat updated: ${deviceId}`);
+        }
+
+        if (eventType === "status") {
+
+            await pool.query(`
+                INSERT INTO devices (
+                    device_id,
+                    power_state,
+                    last_seen
+                )
+                VALUES ($1, $2, NOW())
+                ON CONFLICT (device_id)
+                DO UPDATE SET
+                    power_state = $2,
+                    last_seen = NOW()
+            `, [
+                deviceId,
+                payload.power
+            ]);
+
+            console.log(`[DB] Status updated: ${deviceId}`);
+        }
+
+    } catch (err) {
+        console.error("[MQTT ERROR]", err);
+    }
 });
