1b02c2d7a8fafbe6feb2a23a47a62a8204c63ef0
Antony Juza <antonyjuza@gmail.com>
2026-06-06 16:19:43 +0530

    feat: add device routes for sync, retrieval, and command execution

--- routes/deviceRoutes.js
+++ routes/deviceRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const deviceController = require('../controllers/deviceController');
 
+console.log('[DEBUG] deviceController:', deviceController);
+
 router.post('/sync', deviceController.syncDevice);
 router.get('/:deviceId', deviceController.getDevice);
 router.post('/:deviceId/command', deviceController.sendCommand);
