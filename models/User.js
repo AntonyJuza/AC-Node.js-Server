3703b6a880ccc4619b6925088935646a1216cbc1
Antony Juza <antonyjuza@gmail.com>
2026-06-08 00:56:55 +0530

    feat: create User model with bcrypt password hashing and comparison methods

--- models/User.js
+++ models/User.js
@@ -26,16 +26,13 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Pre-save hook to hash password before saving
-UserSchema.pre('save', async function(next) {
-  const user = this;
-  if (!user.isModified('password')) return next();
-  try {
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
-    next();
-  } catch (error) {
-    return next(error);
+UserSchema.pre('save', async function () {
+  if (!this.isModified('password')) {
+    return;
   }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Method to compare password for login
